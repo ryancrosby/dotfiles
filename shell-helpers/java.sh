7622d50be109f61e09affd31d0088a01614cca7c
Ryan Crosby <ryan.s.crosby@gmail.com>
2021-03-03 05:24:15 -0800

    Fixed java initialization

--- shell-helpers/java.sh
+++ shell-helpers/java.sh
@@ -1,15 +1,18 @@
 # use a function so we don't pollute environment variables
 function setup_java {
-  local home=$(/usr/libexec/java_home)
-  local asdf_java="$HOME/.asdf/java"
-  if [[ -d "$home/bin" ]]; then
+  local home=$(/usr/libexec/java_home) 2> /dev/null
+  local asdf_java="$HOME/.asdf/plugins/java"
+  df_log_debug "checking $home/bin"
+  if [[ -d "$HOME/.asdf/plugins/java" ]]; then
+    df_log_debug "Configuring asdf Java"
+    df_source $HOME/.asdf/plugins/java/set-java-home.zsh
+  elif [[ -n "$home" ]] && [[ -d "$home/bin" ]]; then
+    df_log_debug "Configuring Java Home to $JAVA_HOME"
     export JAVA_HOME="$home"
     export PATH=$JAVA_HOME:$PATH
     export PATH=$JAVA_HOME/bin:$PATH
-  elif [[ -d "$asdf_java" ]]; then
-    echo "asdf java found"
   else
-    echo "Java SDK not found, install by running `java --version`" 
+    df_log_warn "Java SDK not found, install by using asdf java (preferred) or os x system java" 
   fi
 }
-#setup_java
+setup_java

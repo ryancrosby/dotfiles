3f125df4f276b893accaab996b7e91b4b6e64a27
Ryan Crosby <ryan.s.crosby@gmail.com>
2021-05-06 05:38:43 -0700

    java.sh shellcheck

--- shell-helpers/java.sh
+++ shell-helpers/java.sh
@@ -1,20 +1,20 @@
 # use a function so we don't pollute environment variables
 function setup_java {
-  local home=$(/usr/libexec/java_home) 2> /dev/null
-  local asdf_java="$HOME/.asdf/plugins/java"
-  df_log_debug "checking $home/bin"
+  local JHOME
+  JHOME=$(/usr/libexec/java_home) 2> /dev/null
+  df_log_debug "checking $JHOME/bin"
   if [[ -d "$HOME/.asdf/plugins/java" ]]; then
     df_log_debug "Configuring asdf Java"
     if [[ $DF_MYSHELL == "zsh" ]]; then
-      df_source $HOME/.asdf/plugins/java/set-java-home.zsh
+      df_source "$HOME/.asdf/plugins/java/set-java-home.zsh"
     elif [[ $DF_MYSHELL == "bash" ]]; then
-      df_source $HOME/.asdf/plugins/java/set-java-home.bash
+      df_source "$HOME/.asdf/plugins/java/set-java-home.bash"
     else
       df_log_warn "Java is not supported in shell $DF_MYSHELL"
     fi
-  elif [[ -n "$home" ]] && [[ -d "$home/bin" ]]; then
+  elif [[ -n "$JHOME" ]] && [[ -d "$JHOME/bin" ]]; then
     df_log_debug "Configuring Java Home to $JAVA_HOME"
-    export JAVA_HOME="$home"
+    export JAVA_HOME="$JHOME"
     export PATH=$JAVA_HOME:$PATH
     export PATH=$JAVA_HOME/bin:$PATH
   else

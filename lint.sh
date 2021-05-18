5fde747edf110dcb8f23da88f27a80c46682608d
Ryan Crosby <ryan.s.crosby@gmail.com>
2021-05-17 19:20:17 -0700

    better linting

--- lint.sh
+++ lint.sh
@@ -9,7 +9,7 @@ didError=0
 
 function lint_file () {
     # -q for quiet, to disable output. we only care about the exit status
-    if head -n1 "$1" | grep -q '/bin/bash'; then
+  if head -n1 "$1" | grep -q -e '/bin/bash' -e '/bin/sh'; then
     df_log_info "Linting $1 via shellcheck"
     # -S warn, sets the level. lossening it up as i fix things
     if ! shellcheck -x "$1"; then

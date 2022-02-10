cf5c4ad1d7fb6a3aa4e19c232f00ceb993d0730c
Ryan Crosby <ryan.crosby@brandlive.com>
2022-02-10 08:43:41 -0800

    rbenv support

--- shell-helpers/ruby.sh
+++ shell-helpers/ruby.sh
@@ -1,4 +1,18 @@
 # shellcheck shell=bash
 
+if command -v rbenv &> /dev/null ; then
+  df_log_warn "Using rbenv"
+  # eval "$(nodenv init -)"
+  eval "$(rbenv init - zsh)"
+  if [[ $DF_MYSHELL == "zsh" ]]; then
+    eval "$(rbenv init - zsh)"
+  elif [[ $DF_MYSHELL == "bash" ]]; then
+    eval "$(rbenv init - bah)"  
+  else
+    df_log_warn "rbenv may not be supported in $DF_MYSHELL"
+    sleep 5
+  fi
+fi
+
 # ruby aliases
 alias bx="bundle exec"
